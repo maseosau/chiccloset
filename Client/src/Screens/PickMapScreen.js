afb2aa941856f3e546971eed50bcfcb4e8dd6dce
QuocDuy03 <quocduyy.03@gmail.com>
2024-01-04 03:19:55 +0700

    fix pick map

--- Client/src/Screens/PickMapScreen.js
+++ Client/src/Screens/PickMapScreen.js
@@ -1,23 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { View, StyleSheet } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
 import { useMap } from "../contexts/mapContext";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import Colors from "../color";
 
 export default function PickMapScreen() {
-    const {location, setLocation} = useMap();
+    const mapViewRef = useRef(null);
+    const { location, setLocation } = useMap();
     const [nameLocation, setNameLocation] = useState(null);
-    const [initialRegion, setInitialRegion] = useState({
-        latitude: location ? location.latitude : 10.8690162,
-        longitude: location ? location.longitude : 106.80339559,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-    });
-
     const handleMapPress = (event) => {
         setLocation({
             latitude: event.nativeEvent.coordinate.latitude,
             longitude: event.nativeEvent.coordinate.longitude,
         });
+    };
+
+    const moveToLocation = (lat, lng) => {
+        const newRegion = {
+            latitude: lat,
+            longitude: lng,
+            latitudeDelta: 0.0122,
+            longitudeDelta: 0.0121,
+        };
+        mapViewRef.current.animateToRegion(newRegion, 2000)
     };
 
     useEffect(() => {
@@ -42,23 +48,50 @@ export default function PickMapScreen() {
 
     return (
         <View style={styles.container}>
-            <MapView
-                style={styles.map}
-                onPress={handleMapPress}
-                initialRegion={initialRegion}
-                showsUserLocation
-                showsMyLocationButton
-            >
-                {location && (
-                    <Marker
-                        coordinate={{
-                            latitude: location.latitude,
-                            longitude: location.longitude,
-                        }}
-                        title={nameLocation}
-                    />
-                )}
-            </MapView>
+            <View style={styles.searchBar}>
+                <GooglePlacesAutocomplete
+                    fetchDetails
+                    placeholder='Search'
+                    onPress={(data, details = null) => {
+                        const { lat, lng } = details.geometry.location;
+                        setLocation({
+                            latitude: lat,
+                            longitude: lng
+                        })
+                        moveToLocation(lat, lng);
+                    }}
+                    query={{
+                        key: 'AIzaSyDOONue1Xr4ihkImIMKQH4y2E5rnvtlPJ8',
+                        language: 'vi',
+                    }}
+                    onFail={err => console.log(err)}
+                />
+            </View>
+            <View>
+                <MapView
+                    style={styles.map}
+                    ref={mapViewRef}
+                    onPress={handleMapPress}
+                    initialRegion={{
+                        latitude: location ? location.latitude : 10.8690162,
+                        longitude: location ? location.longitude : 106.80339559,
+                        latitudeDelta: 0.0122,
+                        longitudeDelta: 0.0121,
+                    }}
+                    showsUserLocation
+                    showsMyLocationButton
+                >
+                    {location && (
+                        <Marker
+                            coordinate={{
+                                latitude: location.latitude,
+                                longitude: location.longitude,
+                            }}
+                            title={nameLocation}
+                        />
+                    )}
+                </MapView>
+            </View>
         </View>
     );
 }
@@ -66,11 +99,18 @@ export default function PickMapScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     map: {
         width: '100%',
         height: '100%',
     },
+    searchBar: {
+        position: 'absolute',
+        top: 0,
+        width: '100%',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: Colors.gray,
+        zIndex: 1000
+    }
 });
