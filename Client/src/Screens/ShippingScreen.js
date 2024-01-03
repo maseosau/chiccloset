9ba5802b71541e8f34f5f507b0410803c6c774f0
QuocDuy03 <quocduyy.03@gmail.com>
2024-01-04 01:29:22 +0700

    update shipping screen

--- Client/src/Screens/ShippingScreen.js
+++ Client/src/Screens/ShippingScreen.js
@@ -6,6 +6,8 @@ import Btn from "../Components/Btn";
 import { useAuth } from "../contexts/authContext";
 import axios from "axios";
 import { NAME_API } from "../config/ApiConfig";
+import { useMap } from "../contexts/mapContext";
+import * as Location from 'expo-location';
 
 const ShippingInputs = [
   {
@@ -22,29 +24,30 @@ const ShippingInputs = [
   },
 ];
 
+
+
 function ShippingScreen() {
   const route = useRoute();
-  const {userId} = useAuth();
-  const [consignee, setConsignee] = useState([]);
+  const { location, setLocation } = useMap();
+  const { userId } = useAuth();
+  const [consignee, setConsignee] = useState({});
   consignee.products = route.params.data;
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
   const getInformation = async () => {
     try {
       const response = await axios.get(`${NAME_API.LOCALHOST}/getInformation/${userId}`);
       setConsignee(response.data.user);
-  }
-    catch{
+    }
+    catch {
       console.error(error);
       throw error; // Xử lý lỗi ở mức component hoặc nơi gọi hàm getInfomation
     }
   }
-  
-  useEffect(() => {
-    getInformation();
-  }, [])
+
+
 
   const handleInputChange = (index, text, defaultValue) => {
-    if (index ===  0) {
+    if (index === 0) {
       consignee.fullname = text;
       if (text === "") consignee.fullname = defaultValue.fullname;
     }
@@ -58,19 +61,65 @@ function ShippingScreen() {
     }
   }
   const handleSubmit = (consignee) => {
-    if(consignee.phoneNumber === ''){
-      Alert.alert('Invalid phone number','Please enter a phone number')
+    if (consignee.phoneNumber === '') {
+      Alert.alert('Invalid phone number', 'Please enter a phone number')
     }
-    else if(consignee.phoneNumber.length < 10){
-      Alert.alert('Invalid address','Please enter a valid phone number')
+    else if (consignee.phoneNumber.length < 10) {
+      Alert.alert('Invalid address', 'Please enter a valid phone number')
     }
-    else if(consignee.address === ''){
-      Alert.alert('Invalid address','Please enter a address')
+    else if (consignee.address === '') {
+      Alert.alert('Invalid address', 'Please enter a address')
     }
     else {
-      navigation.navigate("PlaceorderScreen", {consignee})
+      navigation.navigate("PlaceorderScreen", { consignee })
     }
   }
+
+  const handleLocateUser = async () => {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      console.log(currentLocation);
+      setLocation({
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+      });
+    } catch (error) {
+      console.error('Error getting location:', error);
+    }
+  };
+
+  useEffect(() => {
+    const getAddressFromCoordinates = async () => {
+      try {
+        const api = 'AIzaSyDOONue1Xr4ihkImIMKQH4y2E5rnvtlPJ8';
+        const response = await fetch(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.latitude},${location.longitude}&key=${api}`
+        );
+        const data = await response.json();
+        if (data.status === 'OK') {
+          setConsignee(prevConsignee => ({
+            ...prevConsignee,
+            address: data.results[0].formatted_address
+          }));
+        }
+      } catch (error) {
+        console.error('Error fetching location details:', error);
+      }
+    };
+
+    location && getAddressFromCoordinates();
+  }, [location]);
+
+  useEffect(() => {
+    setLocation(null);
+    getInformation();
+  }, [])
 
   return (
     <View style={{ flex: 1, paddingTop: 40, backgroundColor: Colors.main }}>
@@ -99,14 +148,40 @@ function ShippingScreen() {
                   }}
                   keyboardType={input.type}
                   underlineColorAndroid="transparent" // for Android
-                  placeholder={index === 0 ? consignee.fullname : (index === 1 ? consignee.phoneNumber : consignee.address)}
+                  value={index === 0 ? consignee.fullname : (index === 1 ? consignee.phoneNumber : consignee.address)}
                   onChangeText={(text) => handleInputChange(index, text, consignee)}
+                  // multiline={false}
                   required
                 />
+                {
+                  input.label === "Address" &&
+                  <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: 10,
+                    marginTop: 10,
+                  }}>
+                    <Btn
+                      icon="location"
+                      bgColor={Colors.blue}
+                      text='Current'
+                      width='37%'
+                      onPress={() => handleLocateUser()}
+                    />
+                    <Btn
+                      icon="map"
+                      bgColor={Colors.blue}
+                      text='Pick'
+                      width='37%'
+                      onPress={() => navigation.navigate('Map')}
+                    />
+                  </View>
+                }
               </View>
             ))}
             <Btn text='CONTINUE' bgColor={Colors.main} color={Colors.white}
-              onPress={() => {handleSubmit(consignee)}}
+              onPress={() => { handleSubmit(consignee) }}
             />
           </View>
         </ScrollView>
